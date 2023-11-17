3b527307b324e1b03871e8b6bbe621ff7c3dc4e2
myychaan <148436364+myychaan@users.noreply.github.com>
2023-11-17 08:56:07 +0100

    Initial Commit

---
+++ Notes-1-NC1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Notes-1-NC1
+//
+//  Created by Mahary Esposito on 17/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Notes-1-NC1/Notes_1_NC1App.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_1_NC1App.swift
+//  Notes-1-NC1
+//
+//  Created by Mahary Esposito on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Notes_1_NC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
